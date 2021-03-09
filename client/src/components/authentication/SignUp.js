28fca6f7ebc56d02d2d8958aba613d51b17cc2d1
will Fletcher <wfletch7@gmail.com>
2021-03-09 13:34:29 -0500

    added semantic integration

--- client/src/components/authentication/SignUp.js
+++ client/src/components/authentication/SignUp.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 
 export default function SignUp() {
-  return <div></div>;
+  return (
+  <div>
+<Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+)
+  </div>)
 }
