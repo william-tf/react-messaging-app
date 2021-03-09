960d2fb00244acd5a0be550ce2f61ff5b99ba800
Seth Bradshaw <seth.bradshaw27@gmail.com>
2021-03-09 15:27:55 -0700

    added login and sign up forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import {ThemeProvider} from '@livechat/ui-kit'
-import {Route, Switch} from 'react-router-dom'
-import Login from './components/authentication/Login'
-import SignUp from './components/authentication/SignUp'
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch, useHistory } from "react-router-dom";
+import Login from "./components/authentication/Login";
+import SignUp from "./components/authentication/SignUp";
 // import Maximized from '../components./Maximized'
 // import Minimized from '../components./Minimized'
-import { ThemeProvider, FixedWrapper, darkTheme, elegantTheme, purpleTheme, defaultTheme } from '@livechat/ui-kit'
+import {
+  ThemeProvider,
+  FixedWrapper,
+  darkTheme,
+  elegantTheme,
+  purpleTheme,
+  defaultTheme,
+} from "@livechat/ui-kit";
 function App() {
+  const { push } = useHistory();
+
   return (
     <div className="App">
       <Switch>
-     <Route path="/login" component={Login}/>
-     <Route path ="/signup" component={SignUp}/>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </div>
   );
--- client/src/components/authentication/Login.js
+++ client/src/components/authentication/Login.js
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Checkbox, Form, Input } from "semantic-ui-react";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 export default function Login() {
-  return <div></div>;
+  const [form, setForm] = useState(initialState);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Credentials ===> ", form);
+    setForm(initialState);
+  };
+
+  return (
+    <div style={{ width: "30%", margin: "10% auto auto auto" }}>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+          <label style={{ textAlign: "left" }}>Email</label>
+          <Input
+            placeholder="Email"
+            name="email"
+            value={form.email}
+            onChange={onChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label style={{ textAlign: "left" }}>Password</label>
+          <Input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={form.password}
+            onChange={onChange}
+          />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </div>
+  );
 }
